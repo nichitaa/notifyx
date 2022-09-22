6c74b4ec92a83763abea957bdeb10b519ed37cba
Nichita Pasecinic <nichittaa@gmail.com>
2022-09-22 22:14:55 +0300

    fix(`durian`): `set_user` plug

--- durian/lib/plugs/set_user.ex
+++ durian/lib/plugs/set_user.ex
@@ -9,13 +9,17 @@ defmodule Durian.Plugs.SetUser do
 
   # to get access to the %User struct use `conn.assigns[:user]`
   def call(conn, _params) do
-    if conn.assigns[:user] === nil do
-      with token when not is_nil(token) <- PlugUtils.get_auth_token(conn),
-           {:ok, user} <- Cache.get_user_from_cache_or_db(token) do
-        assign(conn, :user, user)
+    assign_user =
+      if conn.assigns[:user] === nil do
+        with token when not is_nil(token) <- PlugUtils.get_auth_token(conn),
+             {:ok, user} <- Cache.get_user_from_cache_or_db(token) do
+          {:ok, user}
+        end
       end
-    else
-      conn
+
+    case assign_user do
+      nil -> conn
+      {:ok, user} -> assign(conn, :user, user)
     end
   end
 end
