2f0f8ea4c4a1fd0556283c6d6db68030adf3b0fa
Nichita Pasecinic <nichittaa@gmail.com>
2022-09-23 23:34:33 +0300

    add(`migrations`): more migrations

--- kiwi/priv/repo/migrations/20220923201836_create_users_notifications.exs
+++ kiwi/priv/repo/migrations/20220923201836_create_users_notifications.exs
@@ -3,12 +3,14 @@ defmodule Kiwi.Repo.Migrations.CreateUsersNotifications do
 
   def change do
     create table(:users_notifications, primary_key: false) do
-      add(:id, :binary_id, primary_key: true)
+      add(:notification_id, references(:notifications, type: :binary_id), null: false)
       add(:to_user_id, :binary_id, null: false)
       # sent-seen
       add(:status, :string, null: false)
 
       timestamps()
     end
+
+    create(unique_index(:users_notifications, [:notification_id, :to_user_id]))
   end
 end
